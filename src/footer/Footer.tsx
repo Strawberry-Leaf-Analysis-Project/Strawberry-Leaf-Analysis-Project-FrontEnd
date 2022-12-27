c616087932e6bc2ba7a29ed40472cc13aeda64df
c0c0pang <coc1789@naver.com>
2022-12-27 19:36:22 +0900

    feat: footer 틀 제작

---
+++ src/footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+  FooterDiv,
+  FooterCenter
+} from './styled_footer'
+function Footer() {
+  return (
+    <FooterCenter>
+    <FooterDiv>Footer</FooterDiv>
+    </FooterCenter>
+  )
+}
+
+export default Footer
---
+++ src/footer/styled_footer.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const FooterCenter = styled.div`
+    display: flex;
+    justify-content: center;
+
+`
+
+export const FooterDiv = styled.div`
+    display: flex;
+    align-items: center;
+    bottom: 0;
+    width:80%;
+    height: 100px;
+    /* background-color: red; */
+    border-top: 0.8px solid #454C5C;
+`
