60c75343f6bbee37ee29b1edf49fd55625d16712
c0c0pang <coc1789@naver.com>
2022-12-28 22:25:07 +0900

    feat: 게시판 종류별 출력 기능 추가

--- src/board/Board.tsx
+++ src/board/Board.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {
     BoardDiv,
     CardListDiv,
@@ -6,74 +6,130 @@ import {
     ArrowDiv,
     IconLeftDiv,
     IconRightDiv,
+    EditIcon,
+    EditIconDiv,
+    SortText
 } from './styled_board'
+import axios from 'axios';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import leaves from '../assets/image/leaves.jpg';
+// import Add from '../assets/icons/Add.svg'
+import Edit from '../assets/icons/Edit.svg'
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import { SORT_TIME } from '../api/ApiStorage'
+import {
+    moveLikeLeft,
+    moveLookupLeft,
+    moveTimeLeft
+} from './scroll/ScrollLeft'
+import {
+    moveLikeRight,
+    moveLookupRight,
+    moveTimeRight
+} from './scroll/ScrollRight'
+import ModalBoard from '../modal/ModalBoard';
 function Board() {
+    const [sortTimeBoard, setSortTimeBoard] = useState<any[]>([])
+    const [isModal,setIsModal] = useState<boolean>(false);
+    const onModal =()=>{
+        if(isModal){
+            setIsModal(false)
+        }
+        else{
+            setIsModal(true)
+        }
+    }
+    useEffect(() => {
+        axios
+            .get<any[]>(SORT_TIME)
+            .then((response) => {
+                setSortTimeBoard(response.data)
+                console.log(response.data)
+            })
+    }, [])
+    const scrollTimeRef = useRef<HTMLInputElement>(null)
+    const scrollLookupRef = useRef<HTMLInputElement>(null)
+    const scrollLikeRef = useRef<HTMLInputElement>(null)
     const CardForm = () => {
-        return (<Card sx={{ maxWidth: 260, borderRadius: 2, minWidth: 260 }}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="160"
-                    image={leaves}
-                    alt="green iguana"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h6" component="div" fontFamily="NanumSquareNeo-R" fontSize="17px">
-                        딸기잎
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary" fontFamily="NanumSquareNeo-R" fontSize="11px">
-                        오늘의 딸기 관찰일지..
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>)
+        return (
+            <>
+                {sortTimeBoard.slice(0).reverse().map((element) => (
+                    <CardDiv>
+                        <Card sx={{ maxWidth: 250, borderRadius: 2, minWidth: 250, boxShadow: 'none' }}>
+                            <CardActionArea>
+                                <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image={leaves}
+                                    alt="green iguana"
+                                />
+                                <CardContent>
+                                    <Typography className='title' gutterBottom variant="h6" component="div" fontFamily="NanumSquareNeo-R" fontSize="17px">
+                                        {element.title}
+                                    </Typography>
+                                    <Typography className='contents' variant="body2" color="text.secondary" fontFamily="NanumSquareNeo-R" fontSize="11px">
+                                        {element.contents}
+                                    </Typography>
+                                    <Typography className='name' variant="body1" fontFamily="NanumSquareNeo-R" fontSize="12px" marginTop='10px'>
+                                        작성자: {element.name}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </CardDiv>
+                ))}
+            </>
+        )
     }
     return (
         <>
-
+            <ModalBoard isModal={isModal}/>
             <BoardDiv>
+                <SortText>최신순</SortText>
                 <ArrowDiv>
-                    <IconLeftDiv>
-                        <ArrowCircleLeftIcon fontSize='large' />
+                    <IconLeftDiv onClick={()=> moveTimeLeft(scrollTimeRef)}>
+                        <ArrowCircleLeftIcon fontSize='large' sx={{ color: 'black' }} />
                     </IconLeftDiv>
-                    <CardListDiv>
-                        <CardDiv>
-                            {CardForm()}
-                        </CardDiv>
-                        <CardDiv>
-                            {CardForm()}
-                        </CardDiv>
-                        <CardDiv>
-                            {CardForm()}
-                        </CardDiv>
-                        <CardDiv>
-                            {CardForm()}
-                        </CardDiv>
-                        <CardDiv>
-                            {CardForm()}
-                        </CardDiv>
-                        <CardDiv>
-                            {CardForm()}
-                        </CardDiv>
-                        <CardDiv>
-                            {CardForm()}
-                        </CardDiv>
+                    <CardListDiv ref={scrollTimeRef} >
+                        {CardForm()}
                     </CardListDiv>
-                    <IconRightDiv>
-                        <ArrowCircleRightIcon fontSize='large' />
+                    <IconRightDiv onClick={()=> moveTimeRight(scrollTimeRef)}>
+                        <ArrowCircleRightIcon fontSize='large' sx={{ color: 'black' }} />
                     </IconRightDiv>
                 </ArrowDiv>
-
+                <SortText>조회순</SortText>
+                <ArrowDiv>
+                    <IconLeftDiv onClick={()=> moveLookupLeft(scrollLookupRef)}>
+                        <ArrowCircleLeftIcon fontSize='large' sx={{ color: 'black' }} />
+                    </IconLeftDiv>
+                    <CardListDiv ref={scrollLookupRef} >
+                        {CardForm()}
+                    </CardListDiv>
+                    <IconRightDiv onClick={()=> moveLookupRight(scrollLookupRef)}>
+                        <ArrowCircleRightIcon fontSize='large' sx={{ color: 'black' }} />
+                    </IconRightDiv>
+                </ArrowDiv>
+                <SortText>좋아요순</SortText>
+                <ArrowDiv>
+                    <IconLeftDiv onClick={()=>moveLikeLeft(scrollLikeRef)}>
+                        <ArrowCircleLeftIcon fontSize='large' sx={{ color: 'black' }} />
+                    </IconLeftDiv>
+                    <CardListDiv ref={scrollLikeRef} >
+                        {CardForm()}
+                    </CardListDiv>
+                    <IconRightDiv onClick={()=> moveLikeRight(scrollLikeRef)}>
+                        <ArrowCircleRightIcon fontSize='large' sx={{ color: 'black' }} />
+                    </IconRightDiv>
+                </ArrowDiv>
             </BoardDiv>
-
+            <EditIconDiv onClick={onModal}>
+                <EditIcon src={Edit} />
+            </EditIconDiv>
         </>
     )
 }
