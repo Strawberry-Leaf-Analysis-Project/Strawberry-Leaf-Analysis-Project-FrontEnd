637fd43c71d7b8259c5160d6d6b557bdffe0f9d9
c0c0pang <coc1789@naver.com>
2023-01-09 03:06:56 +0900

    refactor: 모바일 환경에서 로그인,로그아웃 여부에 따라 분리

--- src/userMenu/MenuTitle.ts
+++ src/userMenu/MenuTitle.ts
@@ -1,6 +1,12 @@
-export const MenuTitle: Array<object | any> = [
+
+export const MenuLogInTitle: Array<object | any> = [
     {title: '생장 게시판',url:'/growth_board'},
     {title: '나의 생장 일지',url:'/my_growth_diary'},
     {title: '로그인',url:'/Singin'},
+]
+
+export const MenuLogOutTitle: Array<object | any> = [
+    {title: '생장 게시판',url:'/growth_board'},
+    {title: '나의 생장 일지',url:'/my_growth_diary'},
     {title: '로그아웃',url:'/'},
 ]
