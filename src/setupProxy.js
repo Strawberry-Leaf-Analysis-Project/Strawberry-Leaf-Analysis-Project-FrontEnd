33ddf16de28b8f3b47519a97cb5d8a5b9b9f4103
c0c0pang <coc1789@naver.com>
2023-01-10 19:47:35 +0900

    feat: /member api proxy 추가

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api'
+    '/member'
+    ,
+    createProxyMiddleware({
+      target: 'http://127.0.0.1:8000',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    '/board'
     ,
     createProxyMiddleware({
       target: 'http://127.0.0.1:8000',
