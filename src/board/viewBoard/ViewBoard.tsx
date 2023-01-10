b63623c71fb6a928167bde5769af116c1aaf612e
c0c0pang <coc1789@naver.com>
2023-01-11 03:31:08 +0900

    feat: 상세 페이지 이미지 추가

--- src/board/viewBoard/ViewBoard.tsx
+++ src/board/viewBoard/ViewBoard.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useLocation } from "react-router";
 import {
+  AfterDiv,
+  BeforeDiv,
   DateIdTextDiv,
   DateText,
   ExplainText,
@@ -8,25 +10,54 @@ import {
   ImageAfter,
   ImageBefore,
   ImageDiv,
+  Loding,
+  LodingDiv,
+  TextAfter,
+  TextBefore,
   TextDiv,
   TitleText,
   ViewBoardDiv,
 } from './styled_viewBoard'
+import { GET_BOARD } from '../../api/ApiStorage';
+import { useQuery } from 'react-query';
+import loding from '/Users/cocopang/Desktop/Strawberry-Leaf-Analysis-Project-FrontEnd/src/assets/image/loding.gif'
 function ViewBoard() {
-  const {state} = useLocation()
+  const { state } = useLocation()
+  const get_board = useQuery('get_board', async () => {
+    return await GET_BOARD(state.key)
+  })
+  if (get_board.isLoading) {
+    return (
+      <LodingDiv>
+        <Loding src={loding}></Loding>
+      </LodingDiv>
+    )
+  }
+  else {
+    return (
+      <ViewBoardDiv>
+        <TextDiv>
+          <TitleText>{get_board.data.title}</TitleText>
+          <DateIdTextDiv>
+            <IdText>{get_board.data.user}</IdText>
+            <DateText>{get_board.data.date.substring(0, 10).replace(/-/g, ' . ')}</DateText>
+          </DateIdTextDiv>
+        </TextDiv>
+        <ExplainText>{get_board.data.explain}</ExplainText>
+        <ImageDiv>
+          <BeforeDiv>
+            <TextBefore>Before</TextBefore>
+            <ImageBefore src={get_board.data.image} />
+          </BeforeDiv>
+          <AfterDiv>
+            <TextAfter>After</TextAfter>
+            <ImageAfter src={get_board.data.image} />
+          </AfterDiv>
+        </ImageDiv>
+      </ViewBoardDiv>
+    )
+  }
 
-  return (
-    <ViewBoardDiv>
-      <TextDiv>
-        <TitleText>{state.title}</TitleText>
-        <DateIdTextDiv>
-          <IdText>{state.user}</IdText>
-          <DateText>{state.date.substring(0,10).replace(/-/g,' . ')}</DateText>
-        </DateIdTextDiv>
-      </TextDiv>
-      <ExplainText>{state.explain}</ExplainText>
-    </ViewBoardDiv>
-  )
 }
 
 export default ViewBoard
--- src/board/viewBoard/styled_viewBoard.ts
+++ src/board/viewBoard/styled_viewBoard.ts
@@ -1,13 +1,20 @@
 import styled from "styled-components";
+export const LodingDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+export const Loding = styled.img`
+    width: 100px;
+`
 
 export const ViewBoardDiv = styled.div`
     display: flex;
     flex-direction: column;
-    padding-left: 10%;
-    padding-right: 10%;
+    margin-left: 10%;
+    margin-right: 10%;
     padding-top: 5%;
     gap: 20px;
-    min-height: 100vh;
 `
 export const TextDiv=styled.div`
     display: flex;
@@ -31,13 +38,47 @@ export const IdText=styled.div`
 `
 export const ExplainText = styled.div`
     font-size: 15px;
+    padding-bottom: 10px;
+    border-bottom: 0.5px solid black;
 `
 export const ImageDiv = styled.div`
-    
+    display: flex;
+    justify-content: space-around;
+    gap: 10px;
+`
+export const BeforeDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+export const TextBefore = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
 `
 export const ImageBefore = styled.img`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    margin-bottom: 10px;
+`
 
+export const AfterDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+export const TextAfter = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-weight: bold;
 `
 export const ImageAfter = styled.img`
-    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
 `
