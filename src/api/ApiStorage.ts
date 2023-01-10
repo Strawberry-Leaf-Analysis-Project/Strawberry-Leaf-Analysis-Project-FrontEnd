6c8b242094d39571de0b57fe83ca2f0f519566e5
c0c0pang <coc1789@naver.com>
2023-01-10 19:48:01 +0900

    feat: 각 정렬된 board api 추가

--- src/api/ApiStorage.ts
+++ src/api/ApiStorage.ts
@@ -1,13 +1,23 @@
 
 // export const BASICAPI = 'http://localhost:5000';
 import axios from "axios";
-const api = axios.create({
-    baseURL: '/api/'
-})
-export const MEMBER = '/member/'
+
+const MEMBER = '/member/'
+const BOARD = '/board/'
+const board = axios.create(
+  {
+    baseURL:'/board'
+  }
+)
+const member = axios.create(
+  {
+    baseURL:'/member'
+  }
+)
+
 export const SING_UP = async(id:string,password:string,name:string) =>{
-   await  api
-    .post(MEMBER,{
+   await  member
+    .post('/',{
         id:id,
         password:password,
         name:name
@@ -17,10 +27,9 @@ export const SING_UP = async(id:string,password:string,name:string) =>{
         window.location.replace("/")
     })
 } 
-export const LOGIN = `${MEMBER}login/`
 export const SING_IN = async(id:string,password:string,setLoginCheck:any,setUserKey:any) =>{
     console.log(id,password)
-    await api.post(LOGIN, {
+    await member.post('/login/', {
         id: id,
         password: password
       }
@@ -38,13 +47,37 @@ export const SING_IN = async(id:string,password:string,setLoginCheck:any,setUser
         }
       })
 }
-export const LOGOUT = async(key:number)=>{
-    await api.post(`${MEMBER}${key}/logout/`)
+export const LOGOUT = async()=>{
+    await member.post('/logout/')
     window.localStorage.removeItem('data')
+}
+
+export const CREATE_BOARD = async(title:string,explain:string,image:File)=>{
+  const formData = new FormData()
+  formData.append('title', title);
+  formData.append('explain',explain);
+  formData.append('image',image);
+  await board.post('/',formData).then((res)=>console.log(res))
+}
+export const GET_USER_BOARD = async()=>{
+  return await board.get('/personal_board/').then((res)=>res.data)
+}
+export const GET_LIKE_BOARD = async()=>{
+  return await board.get('/like_board/').then((res)=>res.data)
+}
+export const GET_VIEWS_BOARD = async()=>{
+  return await board.get('/view_board/').then((res)=>res.data)
+}
+export const GET_DATE_BOARD = async()=>{
+  return await board.get('/date_board/').then((res)=>res.data)
+}
+
+export const GET_ALL_BOARD = async()=>{
+  return await board.get('/').then((res)=>res.data)
 }
 export const SORT_TIME = `/sort_time`;
 
 export const USER_DATA = async(key:number) =>{
-  return await api.get(`${MEMBER}${key}`).then((res)=>res.data)
+  return await member.get(`/${key}`).then((res)=>res.data)
 }
 export const MEMBER_IDCHECK = `${MEMBER}/member_idcheck/`
