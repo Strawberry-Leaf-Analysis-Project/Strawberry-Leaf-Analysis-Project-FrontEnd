95ed77fda3957520b45f3d6e3f27597e787ffad3
c0c0pang <coc1789@naver.com>
2023-01-10 21:08:19 +0900

    feat: 상세 페이지 추가

--- src/board/viewBoard/ViewBoard.tsx
+++ src/board/viewBoard/ViewBoard.tsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import { useLocation } from "react-router";
+import {
+  DateIdTextDiv,
+  DateText,
+  ExplainText,
+  IdText,
+  ImageAfter,
+  ImageBefore,
+  ImageDiv,
+  TextDiv,
+  TitleText,
+  ViewBoardDiv,
+} from './styled_viewBoard'
 function ViewBoard() {
   const {state} = useLocation()
+
   return (
-    <div>{state.date}</div>
+    <ViewBoardDiv>
+      <TextDiv>
+        <TitleText>{state.title}</TitleText>
+        <DateIdTextDiv>
+          <IdText>{state.user}</IdText>
+          <DateText>{state.date.substring(0,10).replace(/-/g,' . ')}</DateText>
+        </DateIdTextDiv>
+      </TextDiv>
+      <ExplainText>{state.explain}</ExplainText>
+    </ViewBoardDiv>
   )
 }
 
---
+++ src/board/viewBoard/styled_viewBoard.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const ViewBoardDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding-left: 10%;
+    padding-right: 10%;
+    padding-top: 5%;
+    gap: 20px;
+    min-height: 100vh;
+`
+export const TextDiv=styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`
+export const TitleText=styled.div`
+    font-size: 30px;
+    font-weight: bold;
+`
+export const DateIdTextDiv=styled.div`
+    display: flex;
+    gap: 10px;
+`
+export const DateText=styled.div`
+    font-size: 15px;
+`
+export const IdText=styled.div`
+    font-size: 14px;
+    font-weight: bold;
+`
+export const ExplainText = styled.div`
+    font-size: 15px;
+`
+export const ImageDiv = styled.div`
+    
+`
+export const ImageBefore = styled.img`
+
+`
+export const ImageAfter = styled.img`
+    
+`
