9d30203047ed7a31614a4940e06b4f7f9940b2f8
c0c0pang <coc1789@naver.com>
2023-01-15 01:50:31 +0900

    feat: 검색 해서 넘어간 주소에게 param 제공하는 기능 구현

--- src/navigation/Nav.tsx
+++ src/navigation/Nav.tsx
@@ -8,17 +8,20 @@ import {
   LogoutText,
   SearchDiv,
   SearchImgDiv,
-  SearchText
+  SearchText,
+  SearchSubmit
 } from './styled_nav_desktop'
 import { NavMobileDiv, StyledLinkMobileTitle, MenuMobileDiv, MenuMobileImgDiv } from './styled_nav_mobile'
 import { LOGOUT } from '../api/ApiStorage'
 import Menu from '../assets/icons/Menu.svg'
 import UserMenu from '../userMenu/Menu'
 import { userData } from '../data/userData'
+import { useNavigate } from 'react-router-dom'
 function Nav() {
   const isDesktopOrMobile = useMediaQuery({ query: '(max-width:768px)' });
   const [togle, setTogle] = useState<boolean>(false);
-  const [searchTogle, SetSearchTogle] = useState<boolean>(false);
+  const [serach,setSerach] = useState<string>('');
+  const navigate = useNavigate()
   const MenuTogle = () => {
     if (togle) {
       setTogle(false);
@@ -31,21 +34,27 @@ function Nav() {
     await LOGOUT()
     window.location.replace("/")
   }
-  const onSearch = () => {
-    if (searchTogle) {
-      SetSearchTogle(false);
-    }
-    else {
-      SetSearchTogle(true);
-    }
+  const onSearch= (e:any) =>{
+    e.preventDefault();
+    setSerach(e.target.value)
+  }
+  const onSubmit= (e:any) =>{
+    e.preventDefault();
+    navigate(`/search/${serach}`,{
+      state:{
+          text:serach
+      }
+  })
+  window.location.replace(`./${serach}`)
   }
   return (
     <NavDiv>
       {!isDesktopOrMobile ?
         (<NavDesktopDiv>
           <StyledLinkDesktopTitle to='/'>Paradise Farm</StyledLinkDesktopTitle>
-          <SearchDiv>
-            <SearchText placeholder='제목,작성자로 검색하세요.' />
+          <SearchDiv onSubmit={onSubmit}>
+            <SearchText type='text' placeholder='제목,작성자로 검색하세요.' onChange={onSearch}/>
+            <SearchSubmit type='submit'/>
           </SearchDiv>
           <StyledLinkText to='/growth_board'>생장 게시판</StyledLinkText>
           <StyledLinkText to='/my_growth_diary'>나의 생장 일지</StyledLinkText>
@@ -55,8 +64,9 @@ function Nav() {
         :
         (<NavMobileDiv>
           <StyledLinkMobileTitle to='/' onClick={() => setTogle(false)}>Paradise Farm</StyledLinkMobileTitle>
-          <SearchDiv>
-            <SearchText placeholder='제목,작성자로 검색하세요.' />
+          <SearchDiv onSubmit={onSubmit}>
+            <SearchText placeholder='제목,작성자로 검색하세요.' onChange={onSearch}/>
+            <SearchSubmit type='submit'/>
           </SearchDiv>
           <MenuMobileDiv>
             <MenuMobileImgDiv src={Menu} onClick={MenuTogle}></MenuMobileImgDiv>
