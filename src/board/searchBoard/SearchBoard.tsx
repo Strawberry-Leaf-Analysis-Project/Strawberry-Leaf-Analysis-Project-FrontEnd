a3afdcdea1e484031d31175f2faaa38620e03e0c
c0c0pang <coc1789@naver.com>
2023-01-18 03:44:41 +0900

    feat: 검색 결과 페이지에 대한 스타일 추가
    검색 결과로 나온 페이지에서 높이를 100vh로 설정하여 결과물들이 적게 나옴에 따라 footer가 가까이 붙는 경우를 방지함

--- src/board/searchBoard/SearchBoard.tsx
+++ src/board/searchBoard/SearchBoard.tsx
@@ -5,18 +5,21 @@ import { CardForm } from '../BoardCard'
 import { useMediaQuery } from 'react-responsive';
 import { useQuery } from 'react-query';
 import { SEARCH_DATA } from '../../api/ApiStorage';
+import { SearchDiv } from './search_styled';
 function SearchBoard() {
     const { state } = useLocation()
     const isDesktopOrMobile = useMediaQuery({ query: '(max-width:768px)' });
     console.log(state.text)
     const search_board = useQuery('search_board', async () => {
         return await SEARCH_DATA(state.text)
-        
+
     })
     return (
-        <DiaryBoardListDiv isMedia={isDesktopOrMobile}>
-            {CardForm(search_board)}
-        </DiaryBoardListDiv>
+        <SearchDiv>
+            <DiaryBoardListDiv isMedia={isDesktopOrMobile}>
+                {CardForm(search_board)}
+            </DiaryBoardListDiv>
+        </SearchDiv>
     )
 }
 
---
+++ src/board/searchBoard/search_styled.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+export const SearchDiv = styled.div`
+    height: 100vh;
+    margin-top: 30px;
+    padding: 10px;
+`
