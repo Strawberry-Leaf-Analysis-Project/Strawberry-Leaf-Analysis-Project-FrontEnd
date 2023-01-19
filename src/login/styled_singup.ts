012ee75ea4d733b2aaa07ff622480adbb4c26d81
c0c0pang <coc1789@naver.com>
2023-01-19 23:07:28 +0900

    feat: 이메일 확인하는 버튼의 스타일 작성

--- src/login/styled_singup.ts
+++ src/login/styled_singup.ts
@@ -12,3 +12,22 @@ export const StarTextDiv = styled.div`
     display: flex;
     gap: 5px;
 `
+export const EamilDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+`
+export const EmailSubmit = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 60px;
+    height: 40px;
+    cursor: pointer;
+    background-color: white;
+    border-radius:  5px;
+    font-weight: bold;
+    font-size: 13px;
+
+`
