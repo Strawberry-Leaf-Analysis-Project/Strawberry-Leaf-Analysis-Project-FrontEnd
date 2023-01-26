5484e0422c1f0f0e5d6910e07874b384ddd7f8be
c0c0pang <coc1789@naver.com>
2023-01-27 01:45:44 +0900

    feat: interface 분리하여 통합관리 파일 추가

---
+++ src/type/Interface.ts
@@ -0,0 +1,13 @@
+export interface Diary {
+    name: string,
+    date: string,
+    status: string
+}
+export interface Image {
+    imageFile: File | any,
+    viewUrl: string | any
+}
+export interface Input {
+    title: string,
+    explain: string
+}
