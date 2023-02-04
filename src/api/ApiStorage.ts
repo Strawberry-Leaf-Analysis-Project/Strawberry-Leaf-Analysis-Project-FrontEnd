57d66a495cd10f57f52089aabf5c835983d9114d
c0c0pang <coc1789@naver.com>
2023-02-04 19:48:32 +0900

    feat: write_board api 추가

--- src/api/ApiStorage.ts
+++ src/api/ApiStorage.ts
@@ -1,128 +1,146 @@
-import { Diary, Image, Input } from '../type/Interface'
+import { Diary, Image, Input, Output } from "../type/Interface";
 import axios from "axios";
-const board = axios.create(
-  {
-    baseURL: '/board'
-  }
-)
-const member = axios.create(
-  {
-    baseURL: '/member',
-  }
-)
-const plants_group = axios.create(
-  {
-    baseURL: '/plants_group',
-  }
-)
+const board = axios.create({
+  baseURL: "/board",
+});
+const member = axios.create({
+  baseURL: "/member",
+});
+const plants_group = axios.create({
+  baseURL: "/plants_group",
+});
 class MemberApi {
-  SING_UP = async (id: string, password: string, name: string, email: string) => {
+  SING_UP = async (
+    id: string,
+    password: string,
+    name: string,
+    email: string
+  ) => {
     await member
-      .post('/', {
+      .post("/", {
         id: id,
         password: password,
         name: name,
-        email: email
+        email: email,
       })
       .then((res) => {
-        console.log(res)
-        window.location.replace("/")
+        console.log(res);
+        window.location.replace("/");
+      });
+  };
+  SING_IN = async (
+    id: string,
+    password: string,
+    setLoginCheck: any,
+    setUserKey: any
+  ) => {
+    console.log(id, password);
+    await member
+      .post("/login/", {
+        id: id,
+        password: password,
       })
-  }
-  SING_IN = async (id: string, password: string, setLoginCheck: any, setUserKey: any) => {
-    console.log(id, password)
-    await member.post('/login/', {
-      id: id,
-      password: password
-    }
-    ).then((response) => {
-      console.log(response.data)
-      if (response.data.message === '로그인 성공') {
-        setLoginCheck(true)
-        setUserKey(response.data.key)
-        window.localStorage.setItem('data', JSON.stringify(response.data))
-        window.location.replace("/")
-        // window.location.replace('/')
-      }
-      else {
-        setLoginCheck(false)
-      }
-    })
-  }
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.message === "로그인 성공") {
+          setLoginCheck(true);
+          setUserKey(response.data.key);
+          window.localStorage.setItem("data", JSON.stringify(response.data));
+          window.location.replace("/");
+          // window.location.replace('/')
+        } else {
+          setLoginCheck(false);
+        }
+      });
+  };
   USER_DATA = async (key: number) => {
-    return await member.get(`/${key}`).then((res) => res.data)
-  }
+    return await member.get(`/${key}`).then((res) => res.data);
+  };
   LOGOUT = async () => {
-    await member.post('/logout/')
-    window.localStorage.removeItem('data')
-  }
+    await member.post("/logout/");
+    window.localStorage.removeItem("data");
+  };
 }
 
 class BoardApi {
-  CREATE_BOARD = async (text: Input, image: Image) => {
-    const formData = new FormData()
-    formData.append('input_image', image.imageFile);
-    formData.append('group_name',text.group_name)
-    formData.append('title', text.title);
-    formData.append('explain', text.explain);
-    await board.post('/input_image/', formData).then((res) => console.log(res))
-  }
-  OUTPUT_BOARD = async ()=>{
-    return await board.post('/output_image/').then((res)=> console.log(res.data))
-  }
-  GET_BOARD = async (key: any) => {
-    return await board.get(`/${key}/`).then((res) => res.data)
-  }
+  INPUT_BOARD = async (text: Input, image: Image) => {
+    const formData = new FormData();
+    formData.append("input_image", image.imageFile);
+    formData.append("group_name", text.group_name);
+    // formData.append('title', text.title);
+    // formData.append('explain', text.explain);
+    await board.post("/input_image/", formData).then((res) => console.log(res));
+  };
+  OUTPUT_BOARD = async (text: Input) => {
+    const formData = new FormData();
+    formData.append("group_name", text.group_name);
+    return await board.post("/output_image/", formData).then((res) => res.data);
+  };
+  WRITE_BOARD = async (input: Input, output: Output) => {
+    await board
+      .post("/write_board/", {
+        title: input.title,
+        explain: input.explain,
+        id: output.id,
+      })
+      .then((res) => console.log(res.data));
+  };
+  GET_BOARD = async (id: any) => {
+    return await board.get(`/${id}/`).then((res) => res.data);
+  };
   GET_USER_BOARD = async () => {
-    return await board.get('/personal_board/').then((res) => res.data)
-  }
+    return await board.get("/personal_board/").then((res) => res.data);
+  };
   GET_LIKE_BOARD = async () => {
-    return await board.get('/like_board/').then((res) => res.data)
-  }
+    return await board.get("/like_board/").then((res) => res.data);
+  };
   GET_VIEWS_BOARD = async () => {
-    return await board.get('/view_board/').then((res) => res.data)
-  }
+    return await board.get("/view_board/").then((res) => res.data);
+  };
   GET_DATE_BOARD = async () => {
-    return await board.get('/date_board/').then((res) => res.data)
-  }
+    return await board.get("/date_board/").then((res) => res.data);
+  };
   DELETE_BOARD = async (key: number) => {
-    await board.delete(`${key}`)
-  }
+    await board.delete(`${key}`);
+  };
   GET_ALL_BOARD = async () => {
-    return await board.get('/').then((res) => res.data)
-  }
+    return await board.get("/").then((res) => res.data);
+  };
+  POST_LIKE_BOARD = async (id: string, num: number) => {
+    await board.post(`/${id}/change_board`, {
+      likes: num,
+    });
+  };
   SEARCH_DATA = async (search: string) => {
-    return await board.get(`/search/`, {
-      params: {
-        search: search
-      }
-    }).then((res) => res.data)
-  }
-
+    return await board
+      .get(`/search/`, {
+        params: {
+          search: search,
+        },
+      })
+      .then((res) => res.data);
+  };
 }
-class DiseaseApi {
-
-}
-class PlantsDetailApi {
-
-}
-class PlantsByDiseaseApi {
-
-}
+class DiseaseApi {}
+class PlantsDetailApi {}
+class PlantsByDiseaseApi {}
 class PlantsGroupApi {
-  GET_GROUP = async() => {
-    return await plants_group.get('/').then((res)=>res.data)
-  }
+  GET_GROUP = async () => {
+    return await plants_group.get("/").then((res) => res.data);
+  };
   POST_GROUP = async (data: Diary) => {
-    await plants_group.post('/', {
-      name: data.name,
-      date: data.date,
-      status: data.status,
-    }).then((res) => console.log(res))
-  }
+    await plants_group
+      .post("/", {
+        name: data.name,
+        date: data.date,
+        status: data.status,
+        id: data.id,
+      })
+      .then((res) => console.log(res));
+  };
 }
-export const BOARD_API = new BoardApi()
+export const BOARD_API = new BoardApi();
 
-export const MEMBER_API = new MemberApi()
+export const MEMBER_API = new MemberApi();
 
-export const PLANTS_GROUP_API = new PlantsGroupApi()
+export const PLANTS_GROUP_API = new PlantsGroupApi();
