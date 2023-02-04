8fb00889d02cfec938bbd3746fd193dad19ec1da
c0c0pang <coc1789@naver.com>
2023-02-04 19:49:57 +0900

    feat: 변경된 api 데이터 반영 및 board card 출력 조건을 추가
    image -> input_image로 수정 및 현재 board에 정보중 title이나expain이 null 노출이 안되도록 추가

--- src/board/BoardCard.tsx
+++ src/board/BoardCard.tsx
@@ -7,38 +7,39 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import { useMediaQuery } from 'react-responsive'
-export const CardForm = (data:any) => {
+export const CardForm = (data: any) => {
     const navigate = useNavigate();
     const isDesktopOrMobile = useMediaQuery({ query: '(max-width:768px)' });
     const is_width = isDesktopOrMobile ? 160 : 220
     const is_height = isDesktopOrMobile ? 90 : 140
-    const is_title = isDesktopOrMobile ? '12px': '17px'
-    const is_contents = isDesktopOrMobile ? '7px': '11px'
-    const is_user = isDesktopOrMobile ? '8px': '12px'
-    const goViewPage = (element:any)=>{
-        navigate(`/growth_board/${element.key}/${element.title}`,{
-            state:{
-                key:element.key
+    const is_title = isDesktopOrMobile ? '12px' : '17px'
+    const is_contents = isDesktopOrMobile ? '7px' : '11px'
+    const is_user = isDesktopOrMobile ? '8px' : '12px'
+    const goViewPage = (element: any) => {
+        navigate(`/growth_board/${element.id}/${element.title}`, {
+            state: {
+                id: element.id
             }
         })
     }
-    if(data.data===undefined){
+    console.log(data.data)
+    if (data.data === undefined) {
         return null
     }
-    if(data.isLoading){
-        return <LodingImage src={loding}/>
+    if (data.isLoading) {
+        return <LodingImage src={loding} />
     }
-    else{
+    else {
         return (
             <>
-                {data.data.map((element:any) => (
-                    <CardDiv key={element.id} onClick={()=>goViewPage(element)}>
+                {data.data.map((element: any) => element.title !== null || element.explain !== null ? (
+                    <CardDiv key={element.id} onClick={() => goViewPage(element)}>
                         <Card sx={{ maxWidth: is_width, borderRadius: 2, minWidth: is_width, boxShadow: 'none' }}>
                             <CardActionArea>
                                 <CardMedia
                                     component="img"
                                     height={is_height}
-                                    image={element.image}
+                                    image={element.input_image}
                                     loading='lazy'
                                 />
                                 <CardContent>
@@ -46,7 +47,7 @@ export const CardForm = (data:any) => {
                                         {element.title}
                                     </Typography>
                                     <Typography className='contents' variant="body2" color="text.secondary" fontFamily="NanumSquareNeo-R" fontSize={is_contents}>
-                                        {element.contents}
+                                        {element.explain}
                                     </Typography>
                                     <Typography className='name' variant="body1" fontFamily="NanumSquareNeo-R" fontSize={is_user} marginTop='5px'>
                                         작성자: {element.user}
@@ -55,9 +56,11 @@ export const CardForm = (data:any) => {
                             </CardActionArea>
                         </Card>
                     </CardDiv>
-                ))}
+                ) : (null)
+
+                )}
             </>
-        ) 
+        )
     }
 
 }
