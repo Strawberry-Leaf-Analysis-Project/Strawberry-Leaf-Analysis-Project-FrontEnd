39b84f2cd3176ee93cfff88c54c51625af4a7642
c0c0pang <coc1789@naver.com>
2023-02-14 04:10:46 +0900

    feat: 박스의 내용이 최대 1줄로 제한해둠
    박스의 크기를 일정하게 하기 위함

--- src/board/styled_board.ts
+++ src/board/styled_board.ts
@@ -1,76 +1,74 @@
 import styled from "styled-components";
 export const BoardDiv = styled.div`
-    /* min-height: 100vh; */
-    padding-left: 14px;
-    padding-right: 14px;
-    padding-top: 10px;
-    padding-bottom: 20px;
-`
+  /* min-height: 100vh; */
+  padding-left: 14px;
+  padding-right: 14px;
+  padding-top: 10px;
+  padding-bottom: 20px;
+`;
 export const CardListDiv = styled.div`
-    display: flex;
-    gap: 20px;
-    ::-webkit-scrollbar{
-        display: none;
-    }
-    scroll-behavior: smooth;
-    overflow: auto;
-    scroll-snap-type: x mandatory;
-    
-`
+  display: flex;
+  gap: 20px;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  scroll-behavior: smooth;
+  overflow: auto;
+  scroll-snap-type: x mandatory;
+`;
 export const CardDiv = styled.div`
-    scroll-snap-align: start;
-    .title{
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
-    .contents{
-        display: -webkit-box;
-        overflow: hidden;
-        -webkit-box-orient: vertical;
-        text-overflow: ellipsis;
-        -webkit-line-clamp: 2;
-    }
-    .name{
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
+  scroll-snap-align: start;
+  .title {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .contents {
+    display: -webkit-box;
+    overflow: hidden;
+    -webkit-box-orient: vertical;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 1;
+  }
+  .name {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 
-    scroll-behavior: smooth;
-`
+  scroll-behavior: smooth;
+`;
 export const ArrowDiv = styled.div`
-    display: flex;
-   justify-content: space-between;
-   align-items: center;
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 export const IconLeftDiv = styled.div`
-    margin-right: 10px;
-    cursor: pointer;
-`
+  margin-right: 10px;
+  cursor: pointer;
+`;
 export const IconRightDiv = styled.div`
-    margin-left: 10px;
-    cursor: pointer;
-`
+  margin-left: 10px;
+  cursor: pointer;
+`;
 export const EditIconDiv = styled.div`
-    display: flex;
-    position: fixed;
-    bottom: 5%;
-    right: 5%;
-`
+  display: flex;
+  position: fixed;
+  bottom: 5%;
+  right: 5%;
+`;
 export const EditIcon = styled.img`
-    background-color: #5FB636;
-    border-radius:7px;
-    cursor: pointer;
-`
+  background-color: #5fb636;
+  border-radius: 7px;
+  cursor: pointer;
+`;
 export const SortText = styled.div`
-    margin-bottom: 20px;
-    margin-left: 45px;
-    margin-top: 20px;
-    font-size: 20px;
-    font-family: 'NanumSquareNeo-B';
-
-`
+  margin-bottom: 20px;
+  margin-left: 45px;
+  margin-top: 20px;
+  font-size: 20px;
+  font-family: "NanumSquareNeo-B";
+`;
 export const LodingImage = styled.img`
-    width: 20px;
-`
+  width: 20px;
+`;
