4f02f750ed0f4329bec645bd58cb6c2795f9d6c9
c0c0pang <coc1789@naver.com>
2023-02-14 04:13:15 +0900

    refactor: 주석 삭제 및 font에 bold 추가
    추후에 안쓰는 styled 삭제 예정

--- src/board/styled_diary.ts
+++ src/board/styled_diary.ts
@@ -1,49 +1,48 @@
 import styled from "styled-components";
 export const DiaryDiv = styled.div`
-    height: 100vh;
-`
+  height: 100vh;
+`;
 export const DiaryBoardDiv = styled.div`
-    /* display: flex; */
-    /* flex-direction: column; */
-    /* justify-content: space-between;
-    align-items: center;
-    gap: 20px; */
-    display: grid;
-    justify-items: center;
-    grid-template-columns: repeat(3, 1fr);
-    padding-top: 20px;
-`
+  display: grid;
+  justify-items: center;
+  grid-template-columns: repeat(3, 1fr);
+  padding-top: 20px;
+`;
 export const DiaryContentDiv = styled.div`
-    display: flex;
-`
+  display: flex;
+`;
 export const DiaryDetailDiv = styled.div`
-    display: flex;
-    justify-content: space-between;
-    border-bottom: 0.7px solid gray;
-    padding-bottom: 20px;
-`
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 0.7px solid gray;
+  padding-bottom: 20px;
+`;
 export const DiaryDetailIdNameDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-`
-export const  DiaryDetailBoardText = styled.div`
-    display: flex;
-    align-items: flex-end;
-`
-export const  DiaryDetailNameText = styled.div`
-    font-size: 20px;
-`
-export const  DiaryDetailIdText = styled.div`
-    font-size: 15px;
-`
-export const DiaryBoardListDiv = styled.div<{isMedia:boolean}>`
-    display: grid;
-    grid-template-columns: ${(props)=>props.isMedia ? 'repeat(auto-fill,minmax(160px,1fr))' : 'repeat(auto-fill,minmax(220px,1fr))'};
-    justify-items: center;
-    gap: 10px;
-`    
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+export const DiaryDetailBoardText = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
+export const DiaryDetailNameText = styled.div`
+  font-size: 20px;
+`;
+export const DiaryDetailIdText = styled.div`
+  font-size: 15px;
+`;
+export const DiaryBoardListDiv = styled.div<{ isMedia: boolean }>`
+  display: grid;
+  grid-template-columns: ${(props) =>
+    props.isMedia
+      ? "repeat(auto-fill,minmax(160px,1fr))"
+      : "repeat(auto-fill,minmax(220px,1fr))"};
+  justify-items: center;
+  gap: 10px;
+`;
 
 export const GroupText = styled.div`
-    padding: 10px;
-`
+  padding: 10px;
+  font-weight: bold;
+`;
