0412a068af39efe6df0e622e38f163ce530cccf8
c0c0pang <coc1789@naver.com>
2023-02-14 04:13:52 +0900

    feat: 게시글 상세 내용 줄간격 및 전체 bold 효과 줌

--- src/board/styled_viewBoard.ts
+++ src/board/styled_viewBoard.ts
@@ -1,113 +1,115 @@
 import styled from "styled-components";
 export const LodingDiv = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 export const Loding = styled.img`
-    width: 100px;
-`
+  width: 100px;
+`;
 
 export const ViewBoardDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-left: 10%;
-    margin-right: 10%;
-    padding-top: 5%;
-    gap: 20px;
-`
-export const TextDiv=styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-`
-export const TitleText=styled.div`
-    font-size: 30px;
-    font-weight: bold;
-`
-export const DateIdTextDiv=styled.div`
-    display: flex;
-    gap: 10px;
-`
-export const DateText=styled.div`
-    font-size: 15px;
-`
-export const IdText=styled.div`
-    font-size: 14px;
-    font-weight: bold;
-`
+  display: flex;
+  flex-direction: column;
+  margin-left: 10%;
+  margin-right: 10%;
+  padding-top: 5%;
+  gap: 20px;
+  font-weight: bold;
+`;
+export const TextDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+export const TitleText = styled.div`
+  font-size: 30px;
+`;
+export const DateIdTextDiv = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+export const DateText = styled.div`
+  font-size: 15px;
+`;
+export const IdText = styled.div`
+  font-size: 14px;
+`;
 export const ExplainText = styled.div`
-    font-size: 15px;
-    padding-bottom: 10px;
-    border-bottom: 0.5px solid black;
-`
+  line-height: 1.5;
+  font-size: 18px;
+  padding-bottom: 10px;
+  border-bottom: 0.5px solid black;
+`;
 export const ImageDiv = styled.div`
-    display: flex;
-    justify-content: space-around;
-    gap: 10px;
-`
+  display: flex;
+  justify-content: space-around;
+  gap: 10px;
+`;
 export const BeforeDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
 export const TextBefore = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-weight: bold;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+`;
 export const ImageBefore = styled.img`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    margin-bottom: 10px;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-bottom: 10px;
+`;
 
 export const AfterDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
 export const TextAfter = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-weight: bold;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+`;
 export const ImageAfter = styled.img`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
 export const ViewsText = styled.div`
-    display: flex;
-`
+  display: flex;
+`;
 export const LikesText = styled.div`
-    display: flex;
-`
+  display: flex;
+  font-size: 20px;
+`;
 export const LikeButtonDiv = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-export const LikeImg = styled.img`
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 10px;
+`;
+export const LikeImg = styled.img``;
 export const LikeButton = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 80px;
-    height: 80px;
-    background-color: #FFFFFF;
-    border-radius: 100px;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  height: 80px;
+  background-color: #ffffff;
+  border-radius: 100px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+  transition: all ease 0.4s 0s;
+  cursor: pointer;
+  :hover {
     transition: all ease 0.4s 0s;
-    cursor: pointer;
-    :hover{
-    transition: all ease 0.4s 0s;
-    box-shadow: 0px 0px 80px #FB7575;
-    }
-`
+    box-shadow: 0px 0px 80px #fb7575;
+  }
+`;
