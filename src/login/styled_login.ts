494d55ddd36e95bb5841a3160b06319086d91e44
c0c0pang <coc1789@naver.com>
2023-02-17 07:51:28 +0900

    feat: 로그인 페이지에서 footer 와 간격 조정
    100vh 줘서 footer 영역과 떨어지게 변경

--- src/login/styled_login.ts
+++ src/login/styled_login.ts
@@ -1,103 +1,105 @@
 import styled from "styled-components";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 export const LoginDiv = styled.div`
-    display: grid;
-    justify-content: center;
-    padding-top: 20px;
-`
-export const LoginForm = styled.form`
-
-`
+  display: grid;
+  justify-content: center;
+  padding-top: 20px;
+  height: 100vh;
+`;
+export const LoginForm = styled.form``;
 export const TitleText = styled.div`
-    text-align: center;
-    font-size: 20px;
-    font-weight: bold;
-`
+  text-align: center;
+  font-size: 20px;
+  font-weight: bold;
+`;
 export const LoginCard = styled.div`
-    width: 300px;
-`
+  width: 300px;
+`;
 export const SubTitleDiv = styled.div`
-    display: grid;
-    gap: 10px;
-`
+  display: grid;
+  gap: 10px;
+`;
 
 export const SubTitleText = styled.div`
-    font-size: 12px;
-    font-weight: bold;
-`
+  font-size: 12px;
+  font-weight: bold;
+`;
 export const InputDiv = styled.div`
   display: grid;
   place-content: center;
-  place-items: center;    
+  place-items: center;
   padding: 20px;
   gap: 35px;
-`
-export const TextInput = styled.input<{width:number,height:number}>`
-    width: ${(props)=>props.width}px;
-    height: ${(props)=>props.height}px;
-    -webkit-appearance: none; 
-    -moz-appearance: none;
-    appearance: none;
-    /* box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.3); */
-    background-color: white;
-    border: none;
-    font-family: inherit;
-    font-size: 15px;
-    padding: 10px;
-    border-radius: 4px;
-    ::placeholder{
-        font-size: 10px;
-    }
-    :focus{
+`;
+export const TextInput = styled.input<{ width: number; height: number }>`
+  width: ${(props) => props.width}px;
+  height: ${(props) => props.height}px;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  /* box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.3); */
+  background-color: white;
+  border: none;
+  font-family: inherit;
+  font-size: 15px;
+  padding: 10px;
+  border-radius: 4px;
+  ::placeholder {
+    font-size: 10px;
+  }
+  :focus {
     outline: 1.5px solid #484848;
-    }
-    :hover{
+  }
+  :hover {
     transition: 0.05s;
     outline: 1.5px solid #484848;
- }
-    
-`
+  }
+`;
 export const ButtonInput = styled.input`
-    border: none;
-    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.3);
-    font-family: inherit;
-    width: 200px;
-    height: 40px;
-    cursor: pointer;
-    transition: all ease 0.5s 0s;
-    font-size: 15px;
-    border-radius: 2px;
-    outline: none;
-    background-color: #5FB636;
-    color: white;
- :hover{
+  border: none;
+  box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.3);
+  font-family: inherit;
+  width: 200px;
+  height: 40px;
+  cursor: pointer;
+  transition: all ease 0.5s 0s;
+  font-size: 15px;
+  border-radius: 2px;
+  outline: none;
+  background-color: #5fb636;
+  color: white;
+  :hover {
     border-radius: 8px;
     width: 220px;
     transition: all ease 0.4s 0s;
     font-weight: bold;
- }
-`
+  }
+`;
 export const SignInLinkDiv = styled.div`
-    display: flex;
-    gap: 10px;
-`
+  display: flex;
+  gap: 10px;
+`;
 export const BoundaryText = styled.div`
-    font-size: 12px;
-`
+  font-size: 12px;
+`;
 export const SerachText = styled.div`
-    font-size: 12px;
-    cursor: pointer;
-`
+  font-size: 12px;
+  cursor: pointer;
+`;
 export const SignInLink = styled(Link)`
+  text-decoration: none;
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
     text-decoration: none;
-    &:focus, &:hover, &:visited, &:link, &:active {
-        text-decoration: none;
-    }
-    font-size: 12px;
-    outline: none;
-    color: black;
-    cursor: pointer;
-`   
+  }
+  font-size: 12px;
+  outline: none;
+  color: black;
+  cursor: pointer;
+`;
 export const PasswordCheckText = styled.div`
-    font-size: 11px;
-`
+  font-size: 11px;
+`;
