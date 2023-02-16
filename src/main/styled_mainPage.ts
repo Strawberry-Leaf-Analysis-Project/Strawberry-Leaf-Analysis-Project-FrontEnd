94c562c2cd3bf4e5acfc9eb41aa0929f5e3fc92a
c0c0pang <coc1789@naver.com>
2023-02-17 07:49:55 +0900

    refactor: 메인 페이지 잎 이미지 크기 축소
    40vw -> 30vw

--- src/main/styled_mainPage.ts
+++ src/main/styled_mainPage.ts
@@ -1,27 +1,27 @@
 import styled from "styled-components";
 
 export const FirstText = styled.div`
-    text-align: center;
-    font-size: 25px;
-    margin-bottom: 30px;
-    font-weight: bold;
-`
+  text-align: center;
+  font-size: 25px;
+  margin-bottom: 30px;
+  font-weight: bold;
+`;
 export const ImageDiv = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 20px;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
 export const LableDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
 
 export const Image = styled.img`
-    width: 40vw;
-`
+  width: 30vw;
+`;
 export const LableText = styled.div`
-    font-size: 15px;
-    font-weight: bold;
-`
+  font-size: 15px;
+  font-weight: bold;
+`;
