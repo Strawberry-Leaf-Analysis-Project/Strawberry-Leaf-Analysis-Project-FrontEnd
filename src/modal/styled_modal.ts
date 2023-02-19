01d686fa1e3b8944e4f02d3c26987d5ed1759579
c0c0pang <coc1789@naver.com>
2023-02-20 03:23:53 +0900

    style: 줄간격과 세미콜론 추가

--- src/modal/styled_modal.ts
+++ src/modal/styled_modal.ts
@@ -1,5 +1,5 @@
-import styled, { keyframes,css } from "styled-components";
-import { Link } from 'react-router-dom';
+import styled, { keyframes, css } from "styled-components";
+import { Link } from "react-router-dom";
 export const modal_show = keyframes`
     from {
       opacity: 0;
@@ -7,7 +7,7 @@ export const modal_show = keyframes`
     to {
       opacity: 1;
     }
-`
+`;
 export const modal_close = keyframes`
     from {
       opacity: 1;
@@ -15,45 +15,45 @@ export const modal_close = keyframes`
     to {
       opacity: 0;
     }
-`
+`;
 export const modal = css`
-    visibility: visible;
-    animation: ${modal_show} 0.3s linear;
-`
+  visibility: visible;
+  animation: ${modal_show} 0.3s linear;
+`;
 export const cloose = css`
-    visibility: hidden;
-    animation: ${modal_close} 0.3s linear;
-`
+  visibility: hidden;
+  animation: ${modal_close} 0.3s linear;
+`;
 export const CenterDiv = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
-export const ModalDiv = styled.div<{isModal:boolean}>`
-    display: flex;
-    position: fixed;  
-    height: 200px;
-    width: 280px;
-    background: white;
-    border-radius: 8px;
-    box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.3);
-    top: 30%;
-    z-index: 999;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
-    ${(props)=>props.isModal ?  modal : cloose};
-    transition: all ease 0.3s;
-    padding: 10px;
-`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const ModalDiv = styled.div<{ isModal: boolean }>`
+  display: flex;
+  position: fixed;
+  height: 200px;
+  width: 280px;
+  background: white;
+  border-radius: 8px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.3);
+  top: 30%;
+  z-index: 999;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  ${(props) => (props.isModal ? modal : cloose)};
+  transition: all ease 0.3s;
+  padding: 10px;
+`;
 export const ButtonDiv = styled.div`
   display: flex;
   gap: 20px;
-`
-export const TitleText  = styled.div`
+`;
+export const TitleText = styled.div`
   font-weight: bold;
-`
-export const ConfirmButton = styled(Link)<{isModal:boolean}>`
+`;
+export const ConfirmButton = styled(Link)<{ isModal: boolean }>`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -62,19 +62,23 @@ export const ConfirmButton = styled(Link)<{isModal:boolean}>`
   border-radius: 5px;
   box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.3);
   font-weight: bold;
-  :hover{
+  :hover {
     transition: all ease 0.3s 0s;
-    background-color: #5FB636;
+    background-color: #5fb636;
     color: white;
- }
- color: black;
- ${(props)=>props.isModal ?  modal : cloose};
- text-decoration: none;
-    &:focus, &:hover, &:visited, &:link, &:active {
-        text-decoration: none;
-    }
-`   
-export const CancelButton = styled.div<{isModal:boolean}>`
+  }
+  color: black;
+  ${(props) => (props.isModal ? modal : cloose)};
+  text-decoration: none;
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+`;
+export const CancelButton = styled.div<{ isModal: boolean }>`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -84,14 +88,14 @@ export const CancelButton = styled.div<{isModal:boolean}>`
   box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.3);
   font-weight: bold;
   cursor: pointer;
-  :hover{
+  :hover {
     transition: all ease 0.3s 0s;
     background-color: #515151;
     color: white;
- }
- ${(props)=>props.isModal ?  modal : cloose};
-`
-export const SelectButton = styled.div<{isModal:boolean}>`
+  }
+  ${(props) => (props.isModal ? modal : cloose)};
+`;
+export const SelectButton = styled.div<{ isModal: boolean }>`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -101,26 +105,25 @@ export const SelectButton = styled.div<{isModal:boolean}>`
   box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.3);
   font-weight: bold;
   cursor: pointer;
-  :hover{
+  :hover {
     transition: all ease 0.3s 0s;
     background-color: #515151;
     color: white;
- }
- ${(props)=>props.isModal ?  modal : cloose};
-`
+  }
+  ${(props) => (props.isModal ? modal : cloose)};
+`;
 export const SortationSelect = styled.select`
   padding: 5px;
-  width:  130px;
+  width: 130px;
   height: 30px;
   border-radius: 5px;
-`
-export const SortationOption = styled.option`
-`
+`;
+export const SortationOption = styled.option``;
 export const ContnetText = styled.div`
   margin-top: 10px;
-`
+`;
 export const TextInputDiv = styled.div`
   display: flex;
   gap: 10px;
- margin: 10px;
-`
+  margin: 10px;
+`;
