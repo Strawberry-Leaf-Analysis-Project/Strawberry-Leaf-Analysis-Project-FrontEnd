68c7293454ec0bd06bfc138eac4efad0d2216f0e
c0c0pang <coc1789@naver.com>
2023-02-25 21:10:28 +0900

    feat: 병충해 결과에 따른 segment 출력
    GET_DISEASE에 현재 게시글 아이디를 넘겨주면 해당하는 segment 결과를 가져오며 이를 기반으로 병충해가 존재하면 비정상을 출력하여 segment 사진을 출력 정상이라면 문구만 출력한다

--- src/board/ViewBoard.tsx
+++ src/board/ViewBoard.tsx
@@ -21,9 +21,13 @@ import {
   LikesText,
   LikeButtonDiv,
   LikeButton,
-  LikeImg
+  LikeImg,
+  SegmentDiv,
+  SegmentImg,
+  SegmentImgDiv,
+  SegmentText
 } from './styled_viewBoard'
-import { BOARD_API } from '../api/ApiStorage';
+import { BOARD_API, PLANTS_DETAIL_API } from '../api/ApiStorage';
 import { useQuery } from 'react-query';
 import loding from '../assets/image/loding.gif'
 import { EditIcon, EditIconDiv } from './styled_board';
@@ -43,6 +47,9 @@ function ViewBoard() {
   const get_board_like = useQuery('get_board_like', async () => {
     return await BOARD_API.GET_BOARD_LIKE(state.id)
   })
+  const get_disease = useQuery('get_disease', async () => {
+    return await PLANTS_DETAIL_API.GET_DISEASE(state.id)
+  })
   const idCheck = () => {
     if (userData !== null && userData.id === get_board.data.user) {
       return true
@@ -52,24 +59,24 @@ function ViewBoard() {
     }
   }
   const onLike = () => {
-    if (get_board_like.data == 1 && togle === 0) {
+    if (get_board_like.data === 1 && togle === 0) {
       BOARD_API.PUSH_LIKE(get_board.data.id, '1')
       setTogle(-1)
     }
-    if (get_board_like.data == 1 && togle === -1) {
+    if (get_board_like.data === 1 && togle === -1) {
       BOARD_API.PUSH_LIKE(get_board.data.id, '0')
       setTogle(0)
     }
-    if (get_board_like.data == 0 && togle === 0) {
+    if (get_board_like.data === 0 && togle === 0) {
       BOARD_API.PUSH_LIKE(get_board.data.id, '0')
       setTogle(1)
     }
-    if (get_board_like.data == 0 && togle === 1) {
+    if (get_board_like.data === 0 && togle === 1) {
       BOARD_API.PUSH_LIKE(get_board.data.id, '1')
       setTogle(0)
     }
   }
-  if (get_board.isLoading && get_board_like.isLoading) {
+  if ((get_board.isLoading) || (get_board_like.isLoading) || (get_disease.isLoading)) {
     return (
       <LodingDiv>
         <Loding src={loding}></Loding>
@@ -102,6 +109,19 @@ function ViewBoard() {
               <ImageAfter src={get_board.data.output_image} />
             </AfterDiv>
           </ImageDiv>
+          <SegmentDiv>
+            {get_disease.data.length === 0 ? (<SegmentText>병충해 확인결과 정상입니다.</SegmentText>) : (
+              <>
+                <SegmentText>병충해 확인결과 비정상입니다.</SegmentText>
+                <SegmentImgDiv>
+                  {get_disease.data.map((e: any) => {
+                    return <SegmentImg src={e.leaf_image} />
+                  })}
+                </SegmentImgDiv>
+              </>
+            )}
+
+          </SegmentDiv>
           <LikeButtonDiv>
             <LikeButton onClick={onLike}>
               {get_board_like.data + togle ? (<LikeImg src={Like_1} />) : (<LikeImg src={Like_0} />)}
